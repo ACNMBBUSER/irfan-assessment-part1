80ffa4c1f06217390161a96ace5e884e5d423ddf
irfan zulkefly <irfan.zulkefly@accenture.com>
2024-02-21 18:21:36 +0800

    enable swagger openapi

--- src/main/java/com/irfan/resumebe/Config/SecurityConfig.java
+++ src/main/java/com/irfan/resumebe/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.irfan.resumebe.Config;
 import com.irfan.resumebe.Filter.JwtAuthenticationFilter;
 import com.irfan.resumebe.Service.UserDetailsImpService;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +30,23 @@ public class SecurityConfig {
     private final UserDetailsImpService userDetailsImpService;
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private static final String[] AUTH_WHITELIST = {
+
+            // for Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-ui.html",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/webjars/**",
+
+            // for Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
 
     public SecurityConfig(UserDetailsImpService userDetailsImpService, JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsImpService = userDetailsImpService;
@@ -34,6 +59,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/login/**", "/register/**")
+                                .permitAll()
+                                .requestMatchers(AUTH_WHITELIST)
                                 .permitAll()
                                 .requestMatchers("/api/user/admin_only/**").hasAuthority("ADMIN")
                                 .anyRequest()
@@ -53,6 +80,23 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My Resume REST API")
+                        .description("Description of my Resume API.")
+                        .version("1.0").contact(new Contact().name("Irfan Zulkefly").email( "www.baeldung.com").url("irfanzulkefly144@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
 }
